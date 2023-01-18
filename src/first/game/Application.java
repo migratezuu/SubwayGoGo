01f0ffc1551b2db42d0f61bd4717bfa3d0cf32a4
dongjoo Lee <zxcvb159852@gmail.com>
2023-01-18 17:24:10 +0900

    ReadMe 수정

---
+++ src/first/game/Application.java
@@ -0,0 +1,20 @@
+package first.game;
+
+import java.util.Scanner;
+
+public class Application {
+
+	public static void main(String[] args) {
+	
+
+		Scanner sc = new Scanner(System.in);
+		
+		
+		//Player AbataSue = new Player();
+		
+		
+		
+		
+	}
+
+}
